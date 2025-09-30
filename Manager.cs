39710be6664535c4363e3cdf7db73b77a17fdaf7
PopovDima <legdim17@gmail.com>
2025-09-30 13:41:49 +0530

    Добавьте файлы проекта.

---
+++ Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Popov_Autoservice
+{
+    internal class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
